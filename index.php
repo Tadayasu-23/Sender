8bd64c4ccc76bb1bda25cbaeca9c8ba78b7459ba
Tadayasu <85864420+Tadayasu-23@users.noreply.github.com>
2024-11-11 18:57:50 +0300

    Update and rename sender.php to index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Document</title>
+</head>
+<body>
+   <form action="index.php" method="post">
+      <input type="text" name="name" placeholder="Укажите Имя" required>
+      <input type="text" name="email" placeholder="Укажите e-mail" required>
+      <input type="text" name="Укажите телефон" placeholder="Укажите номер телефона" required>
+      <input type="submit" name="button" value=""Отправить>
+   </form>
 <?php
 $forma_obrashchenya="Форма обращения родителей";
 error_reporting(E_ERROR);
@@ -47,3 +61,5 @@ if (isset($name)&&($button)) {
 
 echo "<p>форма отправлена<p>"
 }?>
+</body>
+</html>
