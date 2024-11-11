4a87080e9061e0584ce903a061d7d857918065ff
Tadayasu <85864420+Tadayasu-23@users.noreply.github.com>
2024-11-11 18:44:55 +0300

    Update sender.php

--- sender.php
+++ sender.php
@@ -1,17 +1,43 @@
 <?php
-$fio = $_POST['fio'];
-$email = $_POST['email'];
-$fio = htmlspecialchars($fio);
-$email = htmlspecialchars($email);
-$fio = urldecode($fio);
-$email = urldecode($email);
-$fio = trim($fio);
-$email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("Tadayasu@yandex.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: Yukino23@yandex.ru \r\n"))
- {     echo "сообщение успешно отправлено";
-} else {
-    echo "при отправке сообщения возникли ошибки";
+$forma_obrashchenya="Форма обращения родителей";
+error_reporting(E_ERROR);
+if (isset($_POST[name])) {
+ $name=$_POST[name];
+ if($name=='') {
+  unset($name);
+ }
+}
+if (isset($_POST[email])) {
+ $email=$_POST[email];
+ if($email=='') {
+  unset($email);
+ }
+}
+if (isset($_POST[phone])) {
+ $phone=$_POST[phone];
+ if($phone=='') {
+  unset($phone);
+ }
+}
+
+if (isset($name)) {
+ $name=stripslashes($name);
+ $name=htmlspecialchars($name);
+}
+if (isset($email)) {
+ $email=stripslashes($email);
+ $email=htmlspecialchars($email);
+}
+if (isset($phone)) {
+ $phone=stripslashes($phone);
+ $phone=htmlspecialchars($phone);
+}
+
+$address="Tadayasu@yandex.ru"
+$note_text="Имя: $name \r\n Почта: $email \r\n Телефон: $phone \r\n "
+
+if (isset($name)) {
+ mail($address, $forma_obrashchenya, $note_text, "Content-type:text/plain; windows-1251");
+
+echo "<p>форма отправлена<p>"
 }?>
